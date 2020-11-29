069e891aedff3410d1c1d167eb5acb66f3c19213
cosmosmrg <calopc.canova@gmail.com>
2020-11-29 19:44:02 +0700

    init

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1606653069,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite3-sheet1.png",
+		"images/sprite3-sheet2.png",
+		"images/sprite3-sheet3.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/sprite8-sheet1.png",
+		"images/sprite8-sheet2.png",
+		"images/sprite9-sheet0.png",
+		"images/sprite9-sheet1.png",
+		"images/sprite10-sheet0.png"
+	]
+}
